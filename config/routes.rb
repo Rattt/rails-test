3f147cb7461d39774f01357c16553e7ced72586f
Ivan Moroz <genom-1990@yandex.ru>
2016-01-17 18:45:25 +0300

    Пишем вид для тестового задания, с использованием переводов, с ипользования форматирования Angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 RailsTest::Application.routes.draw do
-
+  resources :books, only: [:index, :create, :destroy], defaults: { format: :json }
+  resources :books, only: [:edit,:update]
+  root to: "books#index"
+  get 'about/test'
 end
